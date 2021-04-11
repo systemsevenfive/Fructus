aada362919fb449e725998fffaf64595215db83a
Ryan Saunders <42478632+systemsevenfive@users.noreply.github.com>
2021-04-11 19:57:31 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ryan Saunders on 2021-04-11.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
