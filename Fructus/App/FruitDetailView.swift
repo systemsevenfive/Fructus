e82f16e75f5ef6a79bb36ffd72c723c55be6ddd1
Ryan Saunders <42478632+systemsevenfive@users.noreply.github.com>
2021-04-18 15:03:43 -0400

    Added the Fruit Detail View and supporting views

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,67 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Ryan Saunders on 2021-04-18.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    //MARK: - Properties
+    
+    var fruit: Fruit
+    
+    //MARK: - Body
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    //Header
+                    FruitHeaderView(fruit: fruit)
+                    VStack(alignment: .leading, spacing: 20) {
+                        //TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        //NUTRIENTS
+                        
+                        //SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                        
+                    }//: VSTack
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }//: VSTack
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: Scroll
+            .edgesIgnoringSafeArea(.top)
+        } //: Navigation
+    }
+}
+
+//MARK: - Preview
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+            .previewDevice("iPhone 11 Pro")
+    }
+}
